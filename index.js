b619b7655215e6478655f4dea879662b90a9da8b
PVSPReddyWork <120256741+PVSPReddyWork@users.noreply.github.com>
2022-12-15 18:33:23 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3010;
+const path = require('path');
+
+//app.use(express.static('static'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('./public/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
